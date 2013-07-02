ecb48624d5c547bc4196e71315ceb9d615e30212
Nathan Laan <nathanlaan@gmail.com>
2013-07-02 17:53:58 -0600

    Adding new WebLink entity.

---
+++ Mersiv.Lib/Entity/WebLink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mersiv.Lib.Entity
+{
+    class WebLink
+    {
+        public int ID { get; set; }
+        public int AccountID { get; set; }
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string IconURL { get; set; }
+        public int ClickCount { get; set; }
+        public int OtherUserClickCount { get; set; }
+    }
+}
